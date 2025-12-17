28e4630f71cd68f7b42d62eae7f9359fe35f56c2
IsraaAhmed <ia5186738@gmail.com>
2025-12-17 23:06:53 +0200

    p7

--- Task1/problems.js
+++ Task1/problems.js
@@ -4,16 +4,14 @@
 let x = 5;
 let y = 4;
 
-console.log("Given: x = 5, y = 4");
-console.log("");
-console.log("1. x + y =", x + y);                    
-console.log("2. x - y =", x - y);                    
-console.log("3. x * y + 3 =", x * y + 3);            
-console.log("4. x + y * 2 =", x + y * 2);            
-console.log("5. (x + y) * 2 =", (x + y) * 2);        
-console.log("6. x / y =", x / y);                    
-console.log("7. x % y =", x % y);                    
-console.log("8. (x - 2) * (y + 1) =", (x - 2) * (y + 1)); 
+console.log("x + y =", x + y);                    
+console.log("x - y =", x - y);                    
+console.log("x * y + 3 =", x * y + 3);            
+console.log("x + y * 2 =", x + y * 2);            
+console.log("(x + y) * 2 =", (x + y) * 2);        
+console.log("x / y =", x / y);                    
+console.log("x % y =", x % y);                    
+console.log("(x - 2) * (y + 1) =", (x - 2) * (y + 1)); 
 console.log("===============================");
 
 
@@ -23,26 +21,25 @@ let b = "Hello";
 let c = true;
 let d = {name: "Backend"};
 
-console.log("Variable a =", a, "-> Type:", typeof a);         
-console.log("Variable b =", b, "-> Type:", typeof b);         
-console.log("Variable c =", c, "-> Type:", typeof c);         
-console.log("Variable d =", d, "-> Type:", typeof d);         
-console.log("===============================");
+console.log( "Type of :", a , typeof a);         
+console.log( "Type of :", b , typeof b);         
+console.log( "Type of :", c , typeof c);         
+console.log( "Type of :", d , typeof d);         
+console.log("===========================================");
 
 //Problem 3
 x = 7;
 y = 10;
 
-console.log("Given: x = 7, y = 10");
-console.log("");
-console.log("1. x > y =", x > y);           
-console.log("2. x >= y =", x >= y);         
-console.log("3. x < y =", x < y);           
-console.log("4. x <= y =", x <= y);         
-console.log("5. x == y =", x == y);         
-console.log("6. x === y =", x === y);       
-console.log("7. x != y =", x != y);         
-console.log("8. x !== y =", x !== y);       
+
+console.log("x > y =", x > y);           
+console.log("x >= y =", x >= y);         
+console.log("x < y =", x < y);           
+console.log("x <= y =", x <= y);         
+console.log("x == y =", x == y);         
+console.log("x === y =", x === y);       
+console.log("x != y =", x != y);         
+console.log("x !== y =", x !== y);       
 console.log("===============================");
 
 
@@ -50,13 +47,11 @@ console.log("===============================");
 a = true;
 b = false;
 
-console.log("Given: a = true, b = false");
-console.log("");
-console.log("1. a && b =", a && b);         
-console.log("2. a || b =", a || b);         
-console.log("3. !a =", !a);                 
-console.log("4. (a && !b) =", (a && !b));   
-console.log("5. (!a || b) =", (!a || b));   
+console.log("a && b =", a && b);         
+console.log("a || b =", a || b);         
+console.log("!a =", !a);                 
+console.log("(a && !b) =", (a && !b));   
+console.log("(!a || b) =", (!a || b));   
 console.log("===============================");
 
 
@@ -64,55 +59,47 @@ console.log("===============================");
 let num = 20;
 let str = " years old";
 
-console.log("Given: num = 20, str = ' years old'");
-console.log("");
-console.log('1. num + str =', num + str);                           
-console.log('2. "Age: " + num =', "Age: " + num);                   
-console.log('3. "Result = " + (num + 5) =', "Result = " + (num + 5)); 
-console.log('4. "20" + 10 =', "20" + 10);                           
-console.log('5. (20 + 10) + " days" =', (20 + 10) + " days");       
-console.log('6. "JS" + " " + "Course" =', "JS" + " " + "Course");   
-console.log("===============================");
+
+console.log('num + str =', num + str);                           
+console.log('"Age: " + num =', "Age: " + num);                   
+console.log('"Result = " + (num + 5) =', "Result = " + (num + 5)); 
+console.log('"20" + 10 =', "20" + 10);                           
+console.log('(20 + 10) + " days" =', (20 + 10) + " days");       
+console.log('"JS" + " " + "Course" =', "JS" + " " + "Course");   
+console.log("===========================");
 
 
 //Problem 6
 let n = 10;
 console.log("Initial value: n =", n);
-
 n += 5;
-console.log("1. n += 5 -> n =", n);    
-
+console.log(" n += 5 -> n =", n);    
 n -= 3;
-console.log("2. n -= 3 -> n =", n);    
-
+console.log(" n -= 3 -> n =", n);    
 n *= 2;
-console.log("3. n *= 2 -> n =", n);    
-
+console.log(" n *= 2 -> n =", n);    
 n /= 5;
-console.log("4. n /= 5 -> n =", n);    
-
+console.log(" n /= 5 -> n =", n);    
 n %= 4;
-console.log("5. n %= 4 -> n =", n);    
-console.log("===============================");
+console.log(" n %= 4 -> n =", n);    
+console.log("=================================");
 
 
 //Problem 7
 x = 8;
-console.log("Initial value: x =", x);
-console.log("");
 
-console.log("1. ++x =", ++x);           
-console.log("   x is now:", x);
+console.log("++x =", ++x);           
+console.log("x is now:", x);
 
 let temp = x++;
-console.log("2. x++ =", temp);          
-console.log("   x is now:", x);         
-console.log("3. --x =", --x);           
-console.log("   x is now:", x);
+console.log("x++ =", temp);          
+console.log("x is now:", x);         
+console.log("--x =", --x);           
+console.log("x is now:", x);
 
 temp = x--;
-console.log("4. x-- =", temp);          
-console.log("   x is now:", x);         
+console.log("x-- =", temp);          
+console.log("x is now:", x);         
 console.log("===============================");
 
 
@@ -120,10 +107,7 @@ console.log("===============================");
 a = 4;
 b = 2;
 c = "3";
-
-console.log("Given: a = 4, b = 2, c = '3'");
-console.log("");
-console.log("1. a + b + c =", a + b + c);                    
-console.log("2. a + (b + c) =", a + (b + c));                
-console.log("3. (a + b) * Number(c) =", (a + b) * Number(c)); 
-console.log("4. a + b * c =", a + b * c);                    
+console.log(" a + b + c =", a + b + c);                    
+console.log(" a + (b + c) =", a + (b + c));                
+console.log(" (a + b) * Number(c) =", (a + b) * Number(c)); 
+console.log(" a + b * c =", a + b * c);                    
